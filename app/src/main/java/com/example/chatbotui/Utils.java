da01a118efb3b3776b5294792a000b6b83af7330
Afaqrehman98 <62624461+Afaqrehman98@users.noreply.github.com>
2020-09-10 19:19:55 +0500

    Update Utils.java

--- app/src/main/java/com/example/chatbotui/Utils.java
+++ app/src/main/java/com/example/chatbotui/Utils.java
@@ -2,6 +2,6 @@ package com.example.chatbotui;
 
 public class Utils {
 
-    public static final String EMAIL = "afaqrehman98@gmail.com";
-    public static final String PASSWORD = "Jeddah213";
+    public static final String EMAIL = "";
+    public static final String PASSWORD = "";
 }
